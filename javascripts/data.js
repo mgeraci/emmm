011edb57053c9de245b7059fa97119f2c19c4f3b
Katherine Erickson <katherine.erickson@gmail.com>
2014-06-12 23:46:13 -0400

    add movie event data

---
+++ javascripts/data.js
@@ -0,0 +1,192 @@
+events = {
+  1: {
+    'movie': {
+      'year': 1978,
+      'title': 'Gates of Heaven',
+      'subject': 'A pet cemetery in California'
+    }
+    'waffle': {
+      'base': 'arepa',
+      'toppings': 'guacamole, salsa fresca, &amp; cilantro black beans',
+      'country': 'Venezuela'
+    }
+    'drink': {
+      'base': 'guava',
+      'liquor': 'tequila'
+    }
+    'connection': 'California has the largest Latino population of all U.S. ' +
+                  'states (though not be percentage).'
+  },
+
+  2: {
+    'movie': {
+      'year': 1981,
+      'title': 'Vernon, Florida',
+      'subject': 'Eccentrics in this Florida town'
+    }
+    'waffle': {
+      'base': 'waffle',
+      'toppings': 'maple syrup, whipped coconut cream, &amp; "bacon"',
+      'country': 'U.S.A.'
+    }
+    'drink': {
+      'base': 'bloody mary',
+      'liquor': 'vodka'
+    }
+    'connection': 'Waffle House is headquartered in Georgia, ' +
+                  'which borders Florida.'
+  },
+
+  3: {
+    'movie': {
+      'year': 1988,
+      'title': 'The Thin Blue Line',
+      'subject': 'A man on death row in Texas'
+    }
+    'waffle': {
+      'base': 'jalapeno cornbread',
+      'toppings': 'collards &amp; baked beans',
+      'country': 'U.S.A.'
+    }
+    'drink': {
+      'base': 'sweet tea',
+      'liquor': 'bourbon'
+    }
+    'connection': 'Texas.'
+  },
+
+  4: {
+    'movie': {
+      'year': 1991,
+      'title': 'A Brief History of Time',
+      'subject': 'Stephen Hawking'
+    }
+    'waffle': {
+      'base': 'soda bread',
+      'toppings': '"meat" &amp; potato soup',
+      'country': 'Ireland'
+    }
+    'drink': {
+      'name': 'ponzu',
+      'liquor': 'whiskey'
+    }
+    'connection': 'Stephen Hawking is from UK, which is next to Ireland, ' +
+                  'and barely in the Roman Empire (where ponzu is from).'
+  },
+
+  5: {
+    'movie': {
+      'year': 1997,
+      'title': 'Fast, Cheap &amp; Out of Control',
+      'subject': 'Four quirky people'
+    }
+    'waffle': {
+      'base': 'injera',
+      'toppings': 'kik alicha (yellow split peas) &amp; gomen wat (kale)',
+      'country': 'Ethiopia'
+    }
+    'drink': {
+      'base': 'bunna (coffee), hazelnut, &amp; cherry',
+      'liquor': 'beer'
+    }
+    'connection': 'The movie features three animals found in Ethiopia: ' +
+                  'naked mole rats, lions, and giraffes.'
+  },
+
+  6: {
+    'movie': {
+      'year': 1999,
+      'title': 'Mr. Death: The Rise and Fall of Fred A. Leuchter, Jr.',
+      'subject': 'A Holocaust denier'
+    }
+    'waffle': {
+      'base': 'crepe',
+      'toppings': 'raspberry &amp; powdered sugar; pesto &amp; cheese',
+      'country': 'France'
+    }
+    'drink': {
+      'base': 'citrus &amp; chamomile',
+      'liquor': 'champagne'
+    }
+    'connection': 'Germany borders France.'
+  },
+
+  7: {
+    'movie': {
+      'year': 2003,
+      'title': 'The Fog of War: Eleven Lessons from the Life ' +
+               'of Robert S. McNamara',
+      'subject': 'Robert S. McNamara, the Secretary of Defense under Kennedy'
+                 ' and Johnson who escalated U.S. involvement in Vietnam'
+    }
+    'waffle': {
+      'base': 'dosa',
+      'toppings': 'potatoes, salad, &amp; coconut chutney',
+      'country': 'India'
+    }
+    'drink': {
+      'base': 'mango coconut',
+      'liquor': 'vodka'
+    }
+    'connection': 'India opposed American involvement in the Vietnam War.'
+  },
+
+  8: {
+    'movie': {
+      'year': 2008,
+      'title': 'Standard Operating Procedure',
+      'subject': 'The Abu Ghraib prison'
+    }
+    'waffle': {
+      'base': 'roti',
+      'toppings': 'channa (chickpeas) &amp; pumpkin',
+      'country': 'Trinidad'
+    }
+    'drink': {
+      'name': 'switchel',
+      'liquor': 'rum'
+    }
+    'connection': 'In by far the most tenuous connection of this series, ' +
+                  'Tina Fey is a reenactor in the movie, ' +
+                  'and "TINA" is a subset of "TRINIDAD".' +
+                  'Switchel originated in Trinidad.'
+  },
+
+  9: {
+    'movie': {
+      'year': 2010,
+      'title': 'Tabloid',
+      'subject': 'The Manacled Mormon scandal'
+    }
+    'waffle': {
+      'base': 'waffle-fries',
+      'toppings': 'poutine toppings (gravy &amp; cheese curds)',
+      'country': 'Canada'
+    }
+    'drink': {
+      'base': 'raspberry',
+      'liquor': 'gin'
+    }
+    'connection': 'The characters fly into Canada at one point, ' +
+                  'and raspberries grow in Canada.'
+  },
+
+  10: {
+    'movie': {
+      'year': 2013,
+      'title': 'The Unknown Known',
+      'subject': 'Donald Rumsfeld'
+    }
+    'waffle': {
+      'base': 'scallion pancake',
+      'toppings': 'kimchi &amp; sesame tofu',
+      'country': 'Korea'
+    }
+    'drink': {
+      'base': 'shiso',
+      'liquor': 'sake'
+    }
+    'connection': 'Rumsfeld assessed intercontinental ballistic missile ' +
+                  'risk from North Korea, which is close to Japan.'
+  }
+}
